59e675c048d1b1f1fbad289f273565db5adcd2c2
SerbanHiro <serbanvlad19@gmail.com>
2026-01-16 03:58:53 +0200

    comm

--- src/main/kotlin/me/serbob/asteroiddatabasefixer/database/impl/AsteroidDatabase.kt
+++ src/main/kotlin/me/serbob/asteroiddatabasefixer/database/impl/AsteroidDatabase.kt
@@ -8,7 +8,7 @@ abstract class AsteroidDatabase {
     abstract fun connect()
     abstract fun disconnect()
 
-    abstract fun getProfiles(): List<SpoofProfile> // no need for coroutine as it's just one call
+    abstract fun getProfiles(): List<SpoofProfile> // no need for coroutine as it's just one db call
     abstract suspend fun updateSkin(uuid: UUID, profile: SpoofProfile)
     abstract suspend fun removeProfile(profile: SpoofProfile)
 }
